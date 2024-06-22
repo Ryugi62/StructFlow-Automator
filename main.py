00185f6793d2a5fcb39b4d18a2c4b1c660dd6bdc
Ryugi62 <xorjf1027@naver.com>
2024-06-22 11:44:49 +0900

    chore: Refactor window_clicker script and update click coordinates

---
+++ main.py
@@ -0,0 +1,9 @@
+from window_clicker import click_on_child_window
+
+# 사용 예제
+window_title = r"Gen 2024 - [C:\Users\xorjf\Desktop\3764-(에스비일렉트릭)경북 예천군 용궁면 덕계리 380-1, 381 도성기1~5호태양광발전소(축사위)-完\02-(마이다스)구조계산\태양광\[태양광]도성기1~5] - [MIDAS/Gen]"
+child_window_text = "MIDAS/Gen"
+x = 842
+y = 78
+
+click_on_child_window(window_title, child_window_text, x, y)
--- window_clicker.py
+++ window_clicker.py
@@ -56,12 +56,3 @@ def click_on_child_window(window_title, child_window_text, x, y):
             win32api.Sleep(125)
     else:
         print("지정된 이름의 창을 찾을 수 없습니다.")
-
-
-# 사용 예제
-window_title = r"Gen 2024 - [C:\Users\xorjf\Desktop\3764-(에스비일렉트릭)경북 예천군 용궁면 덕계리 380-1, 381 도성기1~5호태양광발전소(축사위)-完\02-(마이다스)구조계산\태양광\[태양광]도성기1~5] - [MIDAS/Gen]"
-child_window_text = "MIDAS/Gen"
-x = 842
-y = 78
-
-click_on_child_window(window_title, child_window_text, x, y)
